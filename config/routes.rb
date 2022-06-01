b24fd1c08c3377f9878881a1a99c6aad48963980
EC2 Default User <ec2-user@ip-172-31-40-136.ec2.internal>
2022-06-01 12:53:06 +0000

    課題8a完了

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'follow'
   end
 
-
+  resources :chats, only: [:show, :create]
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
