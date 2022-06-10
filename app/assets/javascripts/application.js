3c9f5b4bc399378b3c88dc278a13eaa0da842b23
nabeken726 <kenya6910@icloud.com>
2022-06-10 11:04:32 +0000

    グラフ実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,23 @@
-//= require rails-ujs
-//= require activestorage
-//= require jquery 　　　　　
-//= require turbolinks
-//= require_tree .
+
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
+
+import "chartkick/chart.js"
+import Chart from 'chart.js/auto';
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
+
+global.Chart = Chart;
