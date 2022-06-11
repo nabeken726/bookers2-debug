0dbec5b3170100303bcb7cf6ff670e9f42bcc94b
nabeken726 <kenya6910@icloud.com>
2022-06-11 05:39:15 +0000

    応用課題9b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,21 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
+  
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    @book = Book.new
+    if params[:created_at] == ""
+      #if文で分岐させて日付を選択
+      @search_book = "日付を選択してください"
+    else
+      created_at = params[:created_at]
+      #countメソッドで検索してヒットした本を投稿した日付の投稿数を@search_bookで定義
+      @search_book = @books.where(['created_at LIKE ?', "#{created_at}%"]).count
+    end
+  end
+  
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "home/about"=>"homes#about"
-  
+
   # 検索部分
   get "search" => "searches#search_result"
-  
+
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
 
   resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-
+  resources :users, only: [:index,:show,:edit,:update] do
+    get 'search', to: 'users#search'
+  end
 # フォローをネストさせる
   resources :users do
     resource :relationships, only: [:create, :destroy]
